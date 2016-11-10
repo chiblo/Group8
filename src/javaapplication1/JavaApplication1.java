0654b000e4e9b6d60592c0354c280f616f9d506b
Bita Jabbari <bita.jabbari@outlook.com>
2016-11-10 15:29:44 +0100

    Hello I changed it

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -7,7 +7,7 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         for (int i = 0; i < args.length; i++) {
             String arg = args[i];
-            System.out.println("rt");
+            System.out.println("rtfldspuf");
             
         }
     }
