bf7479f2fef1d068255255f4bae81611995454c6
User <User@43TJ8C2>
2016-11-10 15:10:12 +0100

    New commit

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -7,7 +7,7 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         for (int i = 0; i < args.length; i++) {
             String arg = args[i];
-            System.out.println("hghg");
+            System.out.println("rt");
             
         }
     }
